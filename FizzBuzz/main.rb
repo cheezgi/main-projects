16f952dad0cf60f4b64715a2c91c1dbcce5df04d
cheezgi <zack.hixon@hotmail.com>
2013-08-24 08:59:42 -0400

    I felt like showing off my FizzBuzz solution
    just so you know I'm a real programmer

---
+++ FizzBuzz/main.rb
@@ -0,0 +1,36 @@
+#write a program that prints the numbers 1...100.
+#but for the multiples of three, print "fizz" and
+#for the multiples of five, print "buzz". if the
+#number is a multiple of both three and five, print
+#"fizzbuzz"
+
+100.times(){ |i|
+	
+	#i hate to have to do this...
+	i += 1
+	
+	if i % 3 == 0 then print "fizz" end
+	if i % 5 == 0 then print "buzz" end
+	if i % 3 != 0 && i % 5 != 0 then print i end
+	
+	print "\n"
+} #gah, end or }?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
