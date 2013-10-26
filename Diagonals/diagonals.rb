2bc2b3ef2e09d74c5818bf3a6b5a26e12192f0cc
Zack <zack.hixon@hotmail.com>
2013-10-26 11:04:43 -0400

    Update diagonals.rb
    Comments

--- Diagonals/diagonals.rb
+++ Diagonals/diagonals.rb
@@ -1,8 +1,8 @@
 puts "Please enter a number:"
 number = gets.chomp.to_i
 tmp = 2 * (number - 3)
-tmp2 = number - 1
-for i in 4..tmp2
+tmp2 = number - 1 #2 temp variables because I'm not sure if you can do math
+for i in 4..tmp2  #in a for statement
 	tmp = tmp + number - i
 end
 puts tmp
